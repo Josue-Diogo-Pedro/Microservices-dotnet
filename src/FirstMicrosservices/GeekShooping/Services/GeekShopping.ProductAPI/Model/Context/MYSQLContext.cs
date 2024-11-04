e6ee743d2e0d8fa29842221c5a2a5d6429c48676
Josue <pedrojosuediogo@gmail.com>
2024-11-04 04:58:16 -0800

    GeekShopping.ProductAPI - creating firsts models and context

---
+++ src/FirstMicrosservices/GeekShooping/Services/GeekShopping.ProductAPI/Model/Context/MYSQLContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context;
+
+public class MYSQLContext : DbContext
+{
+    public MYSQLContext() { }
+
+    public MYSQLContext(DbContextOptions<MYSQLContext> options) : base(options) { }
+}
